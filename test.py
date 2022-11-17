eaa59b87024716020a2565f33575257792bbc424
indekkusu-era <lkycst@gmail.com>
2022-11-18 03:01:43 +0700

    countdown

--- test.py
+++
@@ -1,8 +0,0 @@
-def factor(num: int):
-    counter = 1
-    a = 10 ** (len(str(num)) - 1)
-    for i, n in enumerate(str(num)):
-        print(a // counter, n)
-        counter *= 10
-
-factor(1324)
